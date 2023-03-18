82f3cf70a7c5c65fe4586313574a8855379678a4
Emily Wairimu <emily.wairimu@student.moringaschool.com>
2023-03-18 17:29:00 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(gamerecord) {
+    const win = gamerecord.find(obj => obj.result === "W");
+    return win ? win.year : undefined;
+  }
+
+  const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+  ]
